af5e25af373688cde063824670413e2c949e76fe
FiddleMe <bryenyeoh1@gmail.com>
2023-09-26 16:59:11 +0800

    create springboot app

---
+++ oop/src/test/java/oop/oop/PortfolioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package oop.oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
