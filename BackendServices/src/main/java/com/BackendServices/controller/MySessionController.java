cd6310ca1a7704bb31dc1009e7552998f513f541
Deric <deric.yeap1@gmail.com>
2023-09-28 12:49:19 +0800

    Example of CRUD api endpoints in controller using service

---
+++ BackendServices/src/main/java/com/BackendServices/controller/MySessionController.java
@@ -0,0 +1,59 @@
+package com.BackendServices.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.BackendServices.entity.MySession;
+import com.BackendServices.service.MySessionService;
+
+import java.util.Optional;
+@RestController
+@RequestMapping("/api/sessions")
+public class MySessionController {
+    
+    @Autowired
+    private MySessionService mySessionService; // Inject the MySessionService
+
+    // Create a new MySession
+    @PostMapping
+    public ResponseEntity<MySession> createMySession(@RequestBody MySession mySession) {
+        MySession createdMySession = mySessionService.createMySession(mySession);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMySession);
+    }
+
+    // Get MySession by ID
+    @GetMapping("/{sessionId}")
+    public ResponseEntity<MySession> getMySessionById(@PathVariable String sessionId) {
+        Optional<MySession> mySessionOptional = mySessionService.getMySessionById(sessionId);
+        if (mySessionOptional.isPresent()) {
+            MySession mySession = mySessionOptional.get();
+            return ResponseEntity.ok(mySession);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Update MySession by ID
+    @PutMapping("/{sessionId}")
+    public ResponseEntity<MySession> updateMySession(@PathVariable String sessionId, @RequestBody MySession updatedMySession) {
+        MySession mySession = mySessionService.updateMySession(sessionId, updatedMySession);
+        if (mySession != null) {
+            return ResponseEntity.ok(mySession);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete MySession by ID
+    @DeleteMapping("/{sessionId}")
+    public ResponseEntity<Void> deleteMySession(@PathVariable String sessionId) {
+        boolean deleted = mySessionService.deleteMySession(sessionId);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Portfolio App for IS442 Project
 
-BACKEND 0. cd Backend Services
+SETUP BACKEND 0. cd BackendServices
 
 1. docker compose up //To start up database
 2. mvn clean install //To start up springboot backend
 3. mvn spring-boot:run
+4. goto localhost:8080, login using default username & password, tables should be created.
