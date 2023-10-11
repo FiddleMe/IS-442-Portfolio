fe31e8923d1835794c8b053a3327f92d2f26a087
YingXuan99 <yxpeeps@gmail.com>
2023-10-11 18:49:44 +0800

    hashed password pt.2

--- BackendServices/src/main/java/com/BackendServices/service/User/LoginOrRegistration.java
+++ BackendServices/src/main/java/com/BackendServices/service/User/LoginOrRegistration.java
@@ -40,37 +40,40 @@ public class LoginOrRegistration {
         return false; // Email is not associated with a user or passwords do not match
     }
 
-
     public User registerUser(String firstName, String lastName, String email, String password) {
         // Check if the email is valid
         if (!isValidEmail(email)) {
             System.out.println("Invalid email format");
             return null;
         }
-
+    
         // Get all users from the UserService
         List<User> users = userService.getAllUsers();
-
+    
         // Check if the email is already in use
         boolean emailInUse = users.stream()
                 .anyMatch(user -> user.getEmail().equals(email));
-
+    
         if (emailInUse) {
             System.out.println("Email already used, please try another Email");
             return null;
         }
-
+    
         // Generate a unique user ID
         String userId = generateUniqueUserId();
-
-        // Create a new user object
+    
+        // Hash the user's password
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+    
+        // Create a new user object with the hashed password
         User newUser = new User();
         newUser.setUserId(userId);
         newUser.setFirstName(firstName);
         newUser.setLastName(lastName);
         newUser.setEmail(email);
-        newUser.setPassword(password);
-
+        newUser.setPassword(hashedPassword); // Store the hashed password
+    
         // Use the createUser method in UserService
         return userService.createUser(newUser);
     }
@@ -78,16 +81,20 @@ public class LoginOrRegistration {
     public boolean changePassword(String email, String newPassword) {
         // Check if the email is associated with a user
         Optional<User> userOptional = userService.getUserByEmail(email);
-
+    
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             
-            // Update the user's password
-            user.setPassword(newPassword);
+            // Hash the new password
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hashedNewPassword = passwordEncoder.encode(newPassword);
+    
+            // Update the user's password with the hashed password
+            user.setPassword(hashedNewPassword);
             
             // Save the updated user to the database
             userService.updateUser(user.getUserId(), user);
-
+    
             return true; // Password updated successfully
         } else {
             // Email is not associated with a user
