01f22401c4695d502dfebe7a33b877f90363eba4
htlee-2021 <105927656+htlee-2021@users.noreply.github.com>
2023-10-16 22:54:38 +0800

    Update error message

--- BackendServices/src/main/java/com/BackendServices/User/UserController.java
+++ BackendServices/src/main/java/com/BackendServices/User/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import com.BackendServices.common.ApiResponse;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,13 +22,13 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable String userId) {
+    public ResponseEntity<ApiResponse> getUserById(@PathVariable String userId) {
         Optional<User> userOptional = userService.getUserById(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), user, "User retrieved successfully"));
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(HttpStatus.NOT_FOUND.value(), null, "User not found"));
         }
     }
     // @PostMapping 
@@ -36,30 +37,32 @@ public class UserController {
     //     return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     // }
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody EmailPasswordRequest loginRequest) {
+    public ResponseEntity<ApiResponse> login(@RequestBody EmailPasswordRequest loginRequest) {
         // Call the validateLogin method in LoginOrRegistration
         boolean loginSuccess = loginOrRegistration.validateLogin(loginRequest.getEmail(), loginRequest.getPassword());
-
         if (loginSuccess) {
-            return ResponseEntity.status(HttpStatus.OK).body("Login successful");
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), "Login successful", "Login successful"));
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse(HttpStatus.UNAUTHORIZED.value(), null, "Login failed"));
         }
     }
 
+
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<ApiResponse> createUser(@RequestBody User user) {
         // Call the registerUser method in LoginOrRegistration
         User createdUser = loginOrRegistration.registerUser(
             user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword()
         );
 
         if (createdUser != null) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse(HttpStatus.CREATED.value(), createdUser, "User created successfully"));
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(HttpStatus.BAD_REQUEST.value(), null, "Failed to create user"));
         }
     }
+
+
     // Test: POST http://localhost:8082/api/users, body: {
     // "email": "newuser@example.com",
     // "password": "asfasfasf",
@@ -67,54 +70,54 @@ public class UserController {
     // "lastName": "Doe"
     // }
     @PutMapping("/updatePassword")
-    public ResponseEntity<String> updatePassword(@RequestBody EmailPasswordRequest updatePasswordRequest) {
+    public ResponseEntity<ApiResponse> updatePassword(@RequestBody EmailPasswordRequest updatePasswordRequest) {
         // Call the changePassword method in LoginOrRegistration
         boolean passwordUpdated = loginOrRegistration.changePassword(updatePasswordRequest.getEmail(), updatePasswordRequest.getPassword());
 
         if (passwordUpdated) {
-            return ResponseEntity.status(HttpStatus.OK).body("Password updated successfully");
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), "Password updated successfully", "Password updated successfully"));
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email is not associated with a user or failed to update password");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(HttpStatus.NOT_FOUND.value(), null, "Email is not associated with a user or failed to update password"));
         }
     }
 
     @PostMapping("/generateOTP")
-    public ResponseEntity<String> generateOTP(@RequestParam String email) {
+    public ResponseEntity<ApiResponse> generateOTP(@RequestParam String email) {
         // Call the generateOTP method in OtpService
         otpService.generateOTP(email);
 
-        return ResponseEntity.status(HttpStatus.OK).body("OTP generated and sent to your email");
+        return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), "OTP generated and sent to your email", "OTP generated and sent to your email"));
     }
 
     @PostMapping("/validateOTP")
-    public ResponseEntity<String> validateOTP(@RequestParam String email, @RequestParam String otp) {
+    public ResponseEntity<ApiResponse> validateOTP(@RequestParam String email, @RequestParam String otp) {
         // Call the validateOTP method in OtpService
         boolean isOTPValid = otpService.validateOTP(email, otp);
 
         if (isOTPValid) {
-            return ResponseEntity.status(HttpStatus.OK).body("OTP is valid");
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), "OTP is valid", "OTP is valid"));
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("OTP is invalid or has expired");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse(HttpStatus.BAD_REQUEST.value(), null, "OTP is invalid or has expired"));
         }
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<User> updateUser(@PathVariable String userId, @RequestBody User updatedUser) {
+    public ResponseEntity<ApiResponse> updateUser(@PathVariable String userId, @RequestBody User updatedUser) {
         User user = userService.updateUser(userId, updatedUser);
         if (user != null) {
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), user, "User updated successfully"));
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(HttpStatus.NOT_FOUND.value(), null, "User not found"));
         }
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable String userId) {
         boolean deleted = userService.deleteUser(userId);
         if (deleted) {
             return ResponseEntity.noContent().build();
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(HttpStatus.NOT_FOUND.value(), null, "User not found"));
         }
     }
 }
