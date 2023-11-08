91c948093989f06f37472b6e1b976e32211f7164
FiddleMe <bryenyeoh1@gmail.com>
2023-11-09 07:15:36 +0800

    add foreign key relationships

--- BackendServices/src/main/java/com/BackendServices/PortfolioStock/PortfolioStocks.java
+++ BackendServices/src/main/java/com/BackendServices/PortfolioStock/PortfolioStocks.java
@@ -1,13 +1,20 @@
 package com.BackendServices.PortfolioStock;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.hibernate.annotations.GenericGenerator;
+
+import com.BackendServices.Portfolio.Portfolio;
+import com.BackendServices.Stock.Stock;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,7 +26,7 @@ public class PortfolioStocks {
   @Column(name = "Portfolio_StockID", length = 36, nullable = false)
   private String portfolioStockId;
 
-  @Column(name = "Portfolio_ID", length = 36)
+  @Column(name = "PortfolioID", length = 36)
   private String portfolioId;
 
   @Column(name = "StockID", length = 36)
@@ -30,6 +37,15 @@ public class PortfolioStocks {
 
   @Column(name = "Quantity")
   private Integer quantity;
+
+
+  @ManyToOne
+  @JoinColumn(name = "PortfolioID", referencedColumnName = "PortfolioID", insertable = false, updatable = false)
+  private Portfolio portfolio;
+
+  @OneToMany(mappedBy = "portfolioStocks")
+  private List<Stock> stock;
+
 
   // Getters and setters
   public String getPortfolioStockId() {
